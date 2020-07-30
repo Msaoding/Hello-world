3fd75793c15a0ec4f0ce6c42e6e44e98b1251c8a
Msaoding <66552322+Msaoding@users.noreply.github.com>
2020-07-30 15:08:43 +0800

    Add files via upload

---
+++ TEST.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define NAME "宋宝"
+#define HOBBY "吹水"
+#define SHIT "吃屎"
+
+int main()
+
+{
+	printf("我的名字是 \n%s\n", NAME);
+	printf("我的兴趣是 \n%s\n", HOBBY);
+	printf("我最喜欢 \n%s\n", SHIT);
+
+	return 0;
+}
