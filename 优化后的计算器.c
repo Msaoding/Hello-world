159e85dcf7a73e20922b08941885cc1391b66be4
Msaoding <66552322+Msaoding@users.noreply.github.com>
2021-07-29 23:24:24 +0800

    Add files via upload

--- 优化后的计算器.c
+++ 优化后的计算器.c
@@ -4,88 +4,87 @@
 void menu(void);
 void choose(void);
 void del(void);
-float input(unsigned short int count, _Bool flag);
-float _sum(void);
-float _mul(void);
-float _div(void);
+double input(unsigned short int count, _Bool flag);
+double _sum(void);
+double _mul(void);
+double _div(void);
 
-int main(void){
+int main(void) {
     choose();
 
-    system("pause");
     return 0;
 }
 
-void del(void){
+void del(void) {
     while (getchar() != '\n');
 }
 
-void menu(void){
+void menu(void) {
     system("cls");
     printf("\n请选择功能:\n");
     printf("a. 加法                     b. 乘法\n");
     printf("c. 除法                     q. 退出\n");
 }
 
-void choose(void){
+void choose(void) {
     char ch = 0;
 
     menu();
-    while ((ch = getchar()) != 'q' && ch != 'Q'){
-        switch (ch){
-            case 'a':
-            case 'A': printf("\nresult: %g\n", _sum()); system("pause"); break;
-            case 'b':
-            case 'B': printf("\nresult: %g\n", _mul()); system("pause"); break;
-            case 'c':
-            case 'C': printf("\nresult: %g\n", _div()); system("pause"); break;
-            default: printf("无效输入，请重试\n");Sleep(1000);
+    while ((ch = getchar()) != 'q' && ch != 'Q') {
+        switch (ch) {
+        case 'a':
+        case 'A': printf("\nresult: %.10lg\n", _sum()); system("pause"); break;
+        case 'b':
+        case 'B': printf("\nresult: %.10lg\n", _mul()); system("pause"); break;
+        case 'c':
+        case 'C': printf("\nresult: %.10lg\n", _div()); system("pause"); break;
+        default: printf("无效输入，请重试\n"); Sleep(1000); del();
         }
 
-        del();
         menu();
     }
 }
 
-float input(unsigned short int count, _Bool flag){
-    float input = 0;
+double input(unsigned short int count, _Bool flag) {
+    double input = 0;
 
     system("cls");
     printf("请输入第 %hu 个数\n", count);
 
-    if (flag){
-        while (scanf("%f", &input) != 1 || input == 0){
+    if (flag) {
+        while (scanf("%lf", &input) != 1 || input == 0) {
             printf("\n输入无效，请重试\n");
             del();
         }
     }
-    else{
-        while (scanf("%f", &input) != 1){
+    else {
+        while (scanf("%lf", &input) != 1) {
             printf("\n输入无效，请重试\n");
             del();
         }
     }
 
+    del();
     return input;
 }
 
-float _sum(void){
-    float num1 = input(1, 0);
-    float num2 = input(2, 0);
+double _sum(void) {
+    double num1 = input(1, 0);
+    double num2 = input(2, 0);
 
     return num1 + num2;
 }
 
-float _mul(void){
-    float num1 = input(1, 0);
-    float num2 = input(2, 0);
+double _mul(void) {
+    double num1 = input(1, 0);
+    double num2 = input(2, 0);
 
     return num1 * num2;
 }
 
-float _div(void){
-    float num1 = input(1, 0);
-    float num2 = input(2, 1);
+double _div(void) {
+    double num1 = input(1, 0);
+    double num2 = input(2, 1);
 
     return num1 / num2;
 }
