30cc5287d3d10f6000fedb581af929bd57d7238d
Msaoding <66552322+Msaoding@users.noreply.github.com>
2021-08-01 14:08:32 +0800

    Add files via upload

---
+++ 转换为二进制.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <windows.h>
+
+void binary(unsigned int);
+
+int main(void){
+    unsigned int num = 0;
+
+    printf("二进制转换\n");
+    while (scanf("%u", &num) == 1){
+        while (getchar() != '\n');
+        binary(num);
+    }
+
+    system("pause");
+    return 0;
+}
+
+void binary(unsigned int num){
+    if (num != 0){
+        printf("%u", (num % 2));
+        binary(num /= 2);
+    }
+    else{
+        putchar('0');
+    }
+    
+    putchar('\n');
+    return;
+}
