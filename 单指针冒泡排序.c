a56d00cc8e546304d3967665c969df17a06995b0
Msaoding <66552322+Msaoding@users.noreply.github.com>
2021-08-10 12:06:55 +0800

    Add files via upload

--- 单指针冒泡排序.c
+++ 单指针冒泡排序.c
@@ -3,7 +3,7 @@
 #include <windows.h>
 #define SZ 10
 
-void sort(int arr[], unsigned int sz);
+void sort(int* arr, unsigned int sz);
 
 int main(void){
     int arr[SZ] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
@@ -23,15 +23,14 @@ int main(void){
     return 0;
 }
 
-void sort (int arr[], unsigned int sz){
-    for (unsigned int count = sz; count > 0; count--){
-        for (unsigned int i = 0; i < count; i++){
-            int* ptr = arr;
+void sort (int* ptr, unsigned sz){
+    for (unsigned count = sz; count > 0; count--){
+        for (unsigned i = 0; i < count; i++){
 
-            if (*(ptr + i) > *(ptr + 1 + i)){
-                *(ptr + i) ^= *(ptr + 1 + i);
-                *(ptr + 1 + i) ^= *(ptr + i);
-                *(ptr + i) ^= *(ptr + 1 + i);
+            if (ptr[i] > ptr[i + 1]){
+                ptr[i] ^= ptr[i + 1];
+                ptr[i + 1] ^= ptr[i];
+                ptr[i] ^= ptr[i + 1];
             }
         }
     }
